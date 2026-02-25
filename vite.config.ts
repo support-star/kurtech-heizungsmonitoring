5e2ee084ef60b4523ab0f4880af7b9c0f97e129e
KurTech Deploy <deploy@kurtech.shop>
2026-02-25 17:09:25 +0000

    fix: base / + stripprefix in traefik

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/dashboard/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
